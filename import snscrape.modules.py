4ad57dbf8704cefd094d18d5fda72bc7349929a9
iwkoiwko <markowicziwo@gmail.com>
2025-04-03 17:04:04 +0200

    03.04.2025

--- import snscrape.modules.py
+++ import snscrape.modules.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 
 NITTER_INSTANCE = "https://nitter.net"
 
-search_query = '"Rafał Trzaskowski" since:2025-03-24'
-url = f"{NITTER_INSTANCE}/search?q={search_query}&f=tweets"
-
+query = '%22Rafa%C5%82+Trzaskowski%22+since%3A2025-03-24'
+url = f"{NITTER_INSTANCE}/search?f=tweets&q={query}"
+print(f"zbieram tweety dla linku: "+url)
 headers = {"User-Agent": "Mozilla/5.0"}
 
 
@@ -14,10 +14,9 @@ response = requests.get(url, headers=headers)
 if response.status_code == 200:
     print("wejscie do if")
     soup = BeautifulSoup(response.text, "html.parser")
-    tweets = soup.find_all("div", class_="tweet-link")
+    tweets = soup.find_all("div", class_="timeline-item")
 
     for i, tweet in enumerate(tweets[:5]):  
         print("wejscie do petli")
         print(f"Tweet {i+1}: {tweet.text.strip()}\n")
-else:
-    print("Failed to fetch tweets. Try a different Nitter instance.")
+
